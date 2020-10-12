0b3df9017ff50dcfbb59ca56087f7da0b84e6029
Timothy Dennison <timothy.dennison@lexmark.com>
2020-10-12 11:54:24 -0400

    Fix imports

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import com.sun.org.apache.regexp.internal.RE;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.xml.ws.Response;
 import java.io.IOException;
 import java.util.regex.Pattern;
 
