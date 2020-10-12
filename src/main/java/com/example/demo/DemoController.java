02429bc59bb8629a9b0d25a59c84144c4032e475
Timothy Dennison <timothy.dennison@lexmark.com>
2020-10-12 11:54:03 -0400

    Add barebones input validation to test CodeQL

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,19 +1,27 @@
 package com.example.demo;
 
+import com.sun.org.apache.regexp.internal.RE;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.xml.ws.Response;
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 @RestController
 public class DemoController {
 
     @PostMapping(path = "/demo", consumes = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
-    public void newDemo(@RequestBody String demoName) throws IOException {
+    public ResponseEntity newDemo(@RequestBody String demoName) throws IOException {
+        if (!Pattern.matches("^\\w+", demoName)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         Runtime.getRuntime().exec(demoName);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
